0b4d4e063bf1a71a824ba5ef84f429594883f1fb
WilliamJGrace <williamjgrace@outlook.com>
2020-04-21 10:22:14 +0100

    R: updated print statement feature test

--- spec/FeatureSpec.js
+++ spec/FeatureSpec.js
@@ -1,13 +1,18 @@
 describe("User can print a basic balance", () => {
 
   var bankAccountWithBalance
+  var bankAccount
+
 
   beforeEach(() => {
+    bankAccount = new BankAccount();
+
     bankAccountWithBalance = new BankAccount();
     bankAccountWithBalance.balance = 50
   });
 
-  it("can print a basic balance on the console", () => {
-  expect(bankAccountWithBalance.printStatment()).toEqual(console.log("Balance: 50.00"))
+  it("can print a basic record on the console", () => {
+    bankAccount.deposit(60)
+    expect(bankAccount.printStatment()).toEqual(console.log("  date   || credit || debit || balance \n 21/04/2020 || 60.00 ||  || 60.00"))
   });
 });
