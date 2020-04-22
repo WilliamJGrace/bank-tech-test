23b815e8038d35186a1d1e5fe9bc1fc81c65c169
WilliamJGrace <williamjgrace@outlook.com>
2020-04-22 16:16:57 +0100

    deposit descreases balance

--- spec/bankAccountSpec.js
+++ spec/bankAccountSpec.js
@@ -4,17 +4,29 @@ describe('bankAccount', () => {
   beforeEach(() => {
 
     recordLog = jasmine.createSpyObj('recordLog', ['create_record'])
+    account = new BankAccount(recordLog)
+
 
   })
 
   it("initilized with a record log", () => {
-    account = new BankAccount(recordLog)
     expect(account.recordLog).toEqual(recordLog)
   })
 
   it('initialises with balance of 0', () => {
    expect(account.getBalance()).toEqual(0)
  })
+
+ describe('#deposit', () => {
+    beforeEach(() => {
+      account.deposit(20)
+    })
+
+    it('can deposit and add to balance', () => {
+      expect(account.getBalance()).toEqual(20)
+    })
+  })
+
 
 
 })
--- src/bankAccount.js
+++ src/bankAccount.js
@@ -9,3 +9,9 @@ BankAccount.prototype.getBalance = function () {
   return this.balance
 
 };
+
+
+BankAccount.prototype.deposit = function (amount) {
+  this.balance += amount
+
+};
