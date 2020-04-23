653d4e28a20420944d5c02a9d3db19c50b077d09
WilliamJGrace <williamjgrace@outlook.com>
2020-04-23 17:50:43 +0100

    record now records date that the record was created

--- spec/RecordSpec.js
+++ spec/RecordSpec.js
@@ -1,5 +1,6 @@
 describe("Record", () => {
   let record
+  let datedouble
   it("records balance of transaction when balance is passed through", () => {
     record = new Record(25, 0, 0)
     expect(record.recordBalance).toEqual(25)
@@ -13,6 +14,22 @@ describe("Record", () => {
   it("records credit of transaction when balance is passed through", () => {
     record = new Record(0, 10, 50)
     expect(record.recordCredit).toEqual(50)
+
+  })
+  it("can record the date when record was created", () => {
+    datedouble = jasmine.createSpyObj('datedouble', ['getDate', 'getMonth', 'getFullYear'])
+    datedouble.getDate.and.callFake(() =>{
+      return 22
+    })
+    datedouble.getMonth.and.callFake(() =>{
+      return 3
+    })
+    datedouble.getFullYear.and.callFake(() =>{
+      return 2020
+    })
+    record = new Record()
+    expect(record.recordCurrentDate(datedouble)).toEqual("22/04/2020")
+
 
   })
 })
--- src/Record.js
+++ src/Record.js
@@ -2,4 +2,17 @@ function Record(balance, debit, credit) {
 this.recordBalance = balance
 this.recordDebit = debit
 this.recordCredit = credit
+this.recordDate = this.recordCurrentDate
+}
+
+
+Record.prototype.recordCurrentDate = function (newDate = new Date()) {
+  var today = newDate
+
+  console.log('TODAY', today)
+  var dd = String(today.getDate()).padStart(2, '0')
+  var mm = String(today.getMonth() + 1).padStart(2, '0') // January is 0!
+  var yyyy = today.getFullYear()
+
+  return dd + '/' + mm + '/' + yyyy
 }
