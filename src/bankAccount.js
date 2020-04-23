bf2ee6e43c13d5ddb677d63e96e599e979786633
WilliamJGrace <williamjgrace@outlook.com>
2020-04-23 18:17:12 +0100

    Some refactoring

--- src/bankAccount.js
+++ src/bankAccount.js
@@ -10,21 +10,23 @@ BankAccount.prototype.getBalance = function () {
 
 };
 
-
-BankAccount.prototype.deposit = function (amount) {
+BankAccount.prototype.isValidTransaction = function (amount) {
   if (!Number.isInteger(amount) || amount > 10000) {
    throw new Error('Please enter a number between 0 and 10000')
  }
+}
 
+
+BankAccount.prototype.deposit = function (amount) {
+
+  this.isValidTransaction(amount)
   this.balance += amount
   return this.recordLog.createRecord(this.balance, null, amount )
 
 };
 
 BankAccount.prototype.withdraw = function (amount) {
-  if (!Number.isInteger(amount) || amount > 10000) {
-   throw new Error('Please enter a number between 0 and 10000')
- }
+  this.isValidTransaction(amount)
  if (this.balance - amount < 0) {
     throw new Error('Withdrawel denied, amount exceeds your current balance')
   }
