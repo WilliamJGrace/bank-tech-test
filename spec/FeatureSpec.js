01855b86c9cf73a90be44399defaa4c0f9554166
WilliamJGrace <williamjgrace@outlook.com>
2020-04-23 11:52:00 +0100

    R: feature test to print a basic record log

--- spec/FeatureSpec.js
+++ spec/FeatureSpec.js
@@ -1,11 +1,15 @@
-// describe('User can print a basic balance', () => {
-//   var bankAccountWithBalance
-//   var bankAccount
-//
-//   beforeEach(() => {
-//     BankAccount.prototype.recordCurrentDate = function () {
-//       return '21/04/2020'
-//     }
+describe('User can print a basic balance', () => {
+  var bank
+  var bankAccountWithBalance
+  var bankAccount
+
+  beforeEach(() => {
+    // BankAccount.prototype.recordCurrentDate = function () {
+    //   return '21/04/2020'
+    // }
+    bank = new Bank();
+    bankAccount = bank.createAccount()
+  })
 //
 //     bankAccount = new BankAccount()
 //
@@ -18,11 +22,11 @@
 //   //   bankAccount.printStatment()
 //   // });
 //
-//   it('can print a few records on the console', () => {
-//     bankAccount.deposit(60)
-//     bankAccount.deposit(20)
-//     bankAccount.withdraw(10)
-//
-//     expect(bankAccount.printStatment()).toEqual('  date   || credit || debit || balance \n21/04/2020 || 60.00 ||  || 60.00 \n21/04/2020 || 20.00 ||  || 80.00 \n21/04/2020 ||  || 10.00 || 70.00 \n')
-//   })
-// })
+  it('can print a few records on the console', () => {
+    bankAccount.deposit(60)
+    bankAccount.deposit(20)
+    bankAccount.withdraw(10)
+
+    expect(bankAccount.printStatment()).toEqual('  date   || credit || debit || balance \n21/04/2020 || 60.00 ||  || 60.00 \n21/04/2020 || 20.00 ||  || 80.00 \n21/04/2020 ||  || 10.00 || 70.00 \n')
+  })
+})
