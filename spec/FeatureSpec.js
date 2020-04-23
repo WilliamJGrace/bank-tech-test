a1fa4598b4ca0b59da6d57a21c2ee111e9f2912c
WilliamJGrace <williamjgrace@outlook.com>
2020-04-23 18:33:57 +0100

    updated feature test so it doesn't rely on date function

--- spec/FeatureSpec.js
+++ spec/FeatureSpec.js
@@ -3,28 +3,18 @@ describe('User can print a basic balance', () => {
   var bankAccount
 
   beforeEach(() => {
-    // BankAccount.prototype.recordCurrentDate = function () {
-    //   return '21/04/2020'
-    // }
     bank = new Bank()
     bankAccount = bank.createAccount()
   })
-  //
-  //     bankAccount = new BankAccount()
-  //
-  //     bankAccountWithBalance = new BankAccount()
-  //     bankAccountWithBalance.balance = 50
-  //   })
-  //
-  //   // it("can print a basic record on the console", () => {
-  //   //   bankAccount.deposit(60)
-  //   //   bankAccount.printStatment()
-  //   // });
-  //
+
   it('can print a few records on the console', () => {
     bankAccount.deposit(60)
     bankAccount.deposit(20)
     bankAccount.withdraw(10)
+    //overiding date function for records
+    for(var i=0;i<bankAccount.recordLog.records.length;i++){
+      bankAccount.recordLog.records[i].recordDate = "23/04/2020"
+    }
 
     expect(bankAccount.printRecords()).toEqual('  date   || credit || debit || balance \n23/04/2020 || 60.00 ||  || 60.00 \n23/04/2020 || 20.00 ||  || 80.00 \n23/04/2020 ||  || 10.00 || 70.00 \n')
   })
